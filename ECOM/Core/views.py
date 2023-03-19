aaf689ac6b74ee4603595e8cd5a08b8806c92b5a
Dips jR <dipsjr.10@Dipss.local>
2023-03-19 00:29:49 +0000

    Project file uploaded as V1

---
+++ ECOM/Core/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from products.models import Category, Item
+
+
+# Create your views here.
+def index(request):
+    items = Item.objects.filter()
+    categories = Category.objects.all()
+
+    return render(request, 'pages/index.html', {
+        'categories': categories,
+        'items': items
+    })
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def cart(request):
+    context = {}
+    return render(request, 'pages/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'pages/checkout.html', context)
